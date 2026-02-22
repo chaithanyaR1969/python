afbd004ce08b1e7bfa1c95f0e48ba16aaf0c55c7
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:09:11 +0530

    Create creation_of_an_object.py

---
+++ creation_of_an_object.py
@@ -0,0 +1,12 @@
+class hero:
+    def __init__(self):
+        self.name="prabhas"
+        self.age=47
+        self.height=6.5
+    def act(self):
+        print("he is a good actor")
+h1=hero()
+print(h1.name)
+print(h1.age)
+print(h1.height)
+h1.act()
