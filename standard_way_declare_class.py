52dccafb5b2ead7ca96456af64d9f2e4acb90a8f
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:20:38 +0530

    Create standard_way_declare_class.py

---
+++ standard_way_declare_class.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self,name,age,gf):
+        self.sname=name
+        self.sage=age
+        self.sgf=gf
+s1=student("rahul",29,"ramya")
+s2=student("shashank",26,"sheela")
+print(s1.sname)
+print(s2.sname)
+print(s1.sage)
+print(s2.sage)
+print(s1.sgf)
+print(s2.sgf)
