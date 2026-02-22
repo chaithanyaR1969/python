c4e18bc40126bad11f0c569c0ee0b76802a5bd91
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:38:43 +0530

    Create instance_method_with_parameter_no_return_value.py

---
+++ instance_method_with_parameter_no_return_value.py
@@ -0,0 +1,14 @@
+class calci:
+    def __init__(self):
+        self.brand="casio"
+        self.color="black"
+    def add(self,a,b):
+        c=a+b
+        print(c)
+
+c1=calci()
+print(c1.brand)
+print(c1.color)
+x=10
+y=20
+c1.add(x,y)
