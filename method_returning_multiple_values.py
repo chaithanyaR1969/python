9f7e76805d011fc46149f50e35cb02a87950da7e
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:49:19 +0530

    Create method_returning_multiple_values.py

---
+++ method_returning_multiple_values.py
@@ -0,0 +1,17 @@
+class calci:
+  def operations(self,a,b):
+    c1=a+b
+    c2=a-b
+    c3=a*b
+    c4=a/b
+    return c1,c2,c3,c4
+c1=calsi()
+x=5
+y=2
+res=c1.operations(x,y)
+print(res)
+r1,r2,,r3,r4=c1.operations(x,y)
+print(r1)
+print(r2)
+print(r3)
+print(r4)
