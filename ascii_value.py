3a39818853e94f88877dff14efe7a2fd42fcf544
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 16:08:28 +0530

    Create ascii_value.py

---
+++ ascii_value.py
@@ -0,0 +1,8 @@
+#to check the ascii value of the alphabet and space
+char=input("enter a alphabet:")
+res=ord(char)
+print(res)
+#to check the alphabet of any ascii value
+num=int(input("enter a number:"))
+res1=chr(num)
+print(res1)
