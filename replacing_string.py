f9f9961d9e76b36673d49a50e768b736c5d43d7b
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 16:11:54 +0530

    Create replacing_string.py

---
+++ replacing_string.py
@@ -0,0 +1,8 @@
+str="abdul is drinking"
+print(str)
+str1=str.replace("is","was")
+print(str1)
+print(str.startswith("abdul"))
+print(str.endswith("drinking"))
+print(str.startswith("om"))
+print(str.endswith("smoking"))
