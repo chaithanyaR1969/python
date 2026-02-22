e564bfa546d01976ceb624b97f26dc8a757fc806
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:59:49 +0530

    Create arithmatic_operations_on_string.py

---
+++ arithmatic_operations_on_string.py
@@ -0,0 +1,16 @@
+str1="bhanu"
+str2="prakash"
+#addition
+str3=str1+str2
+print(str3)
+#subtraction (SHOWS ERROR)
+#str4=str1-str2
+#print(str4)
+#muliplication (SHOWS ERROR)
+#str5=str1*str2
+#print(str5)
+str6=str1*2
+print(str6)
+#dividion (SHOWS ERROR)
+#str7=str1/str2
+#print(str7)
