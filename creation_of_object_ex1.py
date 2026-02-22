c8b32ca6813fd048c43b5f2fbde5f65120b630f5
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-02-22 15:16:44 +0530

    Create creation_of_object_ex1.py

---
+++ creation_of_object_ex1.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self):
+        self.name="ammu"
+        self.age=32
+        self.addr="atp"
+    def study(self):
+        print("ammu is not studing")
+s1=student()
+print(s1.name)
+print(s1.age)
+print(s1.addr)
+s1.study()
