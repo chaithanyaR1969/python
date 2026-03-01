6f3eb1637664fb0167c5c124e56bbb0be3018984
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-03-01 12:53:19 +0530

    Create multiple_inheritance.py

---
+++ oops/inheritance/multiple_inheritance.py
@@ -0,0 +1,13 @@
+class A:
+    def disp_A(self):
+        print("inside A")
+class B:
+    def disp_B(self):
+        print("inside B")
+class C(A,B):
+    def disp_C(self):
+        print("inside C")
+c1=C()
+c1.disp_A()
+c1.disp_B()
+c1.disp_C()
