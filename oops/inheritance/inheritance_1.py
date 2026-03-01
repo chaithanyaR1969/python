539fb2cf0249206f202a099e28110e2839fbe975
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-03-01 11:00:35 +0530

    Create inheritance_1.py

---
+++ oops/inheritance/inheritance_1.py
@@ -0,0 +1,16 @@
+class A:
+  def __init__(self):
+    self.a=10
+class B(A):
+  def __init__(self):
+    A.__init__(self)
+    self.b=20
+class C(B):
+  def __init__(self):
+    B.__init__(self)
+    self.b=30
+
+c1=C()
+print(c1.c)
+print(c1.b)
+print(c1.a)
