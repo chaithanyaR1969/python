c588ff5730ddd70a93460d534173bb4ae52ebf50
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-03-01 13:15:35 +0530

    Create over_riding_method.py
    same method and same number of parameters.in python only over-riding will support and over-loading will not support

---
+++ oops/inheritance/over_riding_method.py
@@ -0,0 +1,13 @@
+class A:
+    def disp(self):
+        print("inside A")
+class B(A):
+    def disp(self):
+        print("inside B")
+class C(B):
+    def disp(self):
+        print("inside C")
+c1=C()
+c1.disp()
+c1.disp()
+c1.disp()
