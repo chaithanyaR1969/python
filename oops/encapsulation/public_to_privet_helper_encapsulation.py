a69801a0c80c4a6709827bb82b97fe01e2a6d511
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-03-01 10:50:54 +0530

    Create public_to_privet_helper_encapsulation.py

---
+++ oops/encapsulation/public_to_privet_helper_encapsulation.py
@@ -0,0 +1,9 @@
+class person:
+    def __init__(self):
+        self.name="Rahul"
+    def __dance(self):
+        print("rahul is good dancer")
+    def helper(self):
+        self.__dance()
+p1= person()
+p1.helper()
