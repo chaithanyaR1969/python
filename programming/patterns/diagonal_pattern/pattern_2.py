967562453433d60ac4eaa9d39c7b5fd2c13d7bee
chaithanyaR1969 <120544810+chaithanyaR1969@users.noreply.github.com>
2026-03-25 10:11:01 +0530

    Add files via upload

---
+++ programming/patterns/diagonal_pattern/pattern_2.py
@@ -0,0 +1,10 @@
+n=5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==j or j==1 or i==n:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+
+
